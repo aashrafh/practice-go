a6f69f55d8a2271c9c208ad799e1a3ca4f165ac7
Ahmed Ashraf <ahmed.ashraf.cmp@gmail.com>
2021-04-23 04:10:09 +0200

    Read the file, covert to lowercase, and split at spaces

--- wordcount/main.go
+++ wordcount/main.go
@@ -2,35 +2,53 @@ package main
 
 import (
 	"bufio"
+	"fmt"
 	"io"
+	"log"
 	"os"
+	"strings"
 )
 
-func processInput(filename string) chan string {
-	output := make(chan string)
+const totalDivisions = 5
 
-	go func() {
-		file, err := os.Open(filename)
-		if err != nil {
-			return
+func processInput(filename string) {
+	file, err := os.Open(filename)
+	if err != nil {
+		log.Fatal(err)
+		os.Exit(1)
+	}
+	defer func() {
+		if err = file.Close(); err != nil {
+			log.Fatal(err)
 		}
-		defer file.Close()
-
-		reader := bufio.NewReader(file)
-
-		for {
-			line, err := reader.ReadString('\n')
-			output <- line
-			if err == io.EOF {
-				break
-			}
-		}
-
-		close(output)
 	}()
-	return output
+
+	text := ""
+
+	buf := make([]byte, 32*1024)
+	reader := bufio.NewReader(file)
+
+	for {
+		n, err := reader.Read(buf)
+		if err != nil {
+			if err != io.EOF {
+				log.Fatal(err)
+			}
+			break
+		}
+		text += string(buf[:n])
+	}
+
+	text = strings.ToLower(text)
+	words := strings.Split(text, " ")
+	chunkSize := len(words)/5
+
+	// for _, v := range words {
+	// 	fmt.Println(v)
+	// }
+	fmt.Printf("Total size %v and division %v and the remainder %v\n", len(words), len(words)/5, len(words)%5)
 }
 
 func main() {
-
+	processInput("ex_input.txt")
 }
