1639bc17c1645aa0536eb407720d4a02607245e2
Ahmed Ashraf <ahmed.ashraf.cmp@gmail.com>
2021-07-22 21:17:28 +0200

    Connect to mongodb

---
+++ seeker/go.mod
@@ -0,0 +1,12 @@
+module seeker
+
+go 1.16
+
+require (
+	github.com/andybalholm/brotli v1.0.3 // indirect
+	github.com/gofiber/fiber/v2 v2.15.0 // indirect
+	github.com/klauspost/compress v1.13.1 // indirect
+	github.com/valyala/fasthttp v1.28.0 // indirect
+	go.mongodb.org/mongo-driver v1.7.0 // indirect
+	golang.org/x/sys v0.0.0-20210630005230-0f9fa26af87c // indirect
+)
--- seeker/main.go
+++ seeker/main.go
@@ -1,5 +1,33 @@
 package main
 
+import (
+	"context"
+	"time"
+
+	"github.com/gofiber/fiber/v2"
+	"github.com/gofiber/fiber/v2/middleware/cors"
+	"go.mongodb.org/mongo-driver/bson/primitive"
+	"go.mongodb.org/mongo-driver/mongo"
+	"go.mongodb.org/mongo-driver/mongo/options"
+)
+
+type Product struct {
+	Id          primitive.ObjectID `json:"_id,omitempty bson:"_id,omitempty"`
+	Title       string             `json:"title,omitempty bson:"title,omitempty"`
+	Description string             `json:"description,omitempty bson:"description,omitempty"`
+	Image       string             `json:"image,omitempty bson:"image,omitempty"`
+	Price       int                `json:"price,omitempty bson:"price,omitempty"`
+}
+
 func main() {
-	
+	ctx, _ := context.WithTimeout(context.Background(), 10*time.Second)
+	client, _ := mongo.Connect(ctx, options.Client().ApplyURI("mongodb://localhost:27017"))
+	db := client.Database("seeker").Collection("products")
+
+	app := fiber.New()
+
+	app.Use(cors.New())
+
+	app.Listen(":3000")
+
 }
